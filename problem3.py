f696647b113df8528b1ebdfbabfa6389e76edfb8
Mattéo Giraud <matteo.giraud@unil.ch>
2025-10-01 11:23:30 +0000

    solve prblm 3

--- problem3.py
+++ problem3.py
@@ -6,7 +6,7 @@ Analyze a list of numbers provided by the user.
 def get_numbers_from_user():
     """
     Get numbers from user until they type 'done'.
-    Return a list of numbers.
+    Return a list of  numbers.
 
     Returns:
         list: List of numbers entered by user
